e9b6c12a36901b1555b7f48e129af89bc0c97540
added stat color <littleseon@gmail.com>
2023-11-27 10:26:57 -0400

    Added Google and WhatsApp login options

--- Screens/ProfileScreen.tsx
+++ Screens/ProfileScreen.tsx
@@ -1,106 +1,106 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
-
-const { height } = Dimensions.get('window'); // Get the height of the device screen
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
 
 const ProfileScreen: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLoginPress = () => {
-    if (!isLoggedIn) {
-    // Navigate to login screen logic here
-    }
-  };
-
-  const handleCreateAccountPress = () => {
-    // Navigate to create account screen logic here
-  };
-
-  const handleLogoutPress = () => {
-    // Logout logic here
-  };
-
-  const dashboardHeight = height * 0.3; // Dashboard takes up 30% of the top half of the screen
 
   return (
-    <ScrollView style={styles.scrollView}>
-      <View style={[styles.dashboardContainer, { height: dashboardHeight }]}>
-        <TouchableOpacity style={styles.profileContainer} onPress={handleLoginPress}>
-          <Image source={{ uri: 'path-to-avatar-image' }} style={styles.avatar} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.welcomeText}>Welcome</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+      </View>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      <View style={styles.socialLoginContainer}>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image source={require('../images/TwitterLogo.png')} style={styles.socialIcon} />
         </TouchableOpacity>
-        {isLoggedIn ? (
-          <Text style={styles.dashboardText}>Dashboard</Text>
-        ) : (
-          <TouchableOpacity onPress={handleLoginPress} style={styles.loginButton}>
-            <Text style={styles.loginButtonText}>Login</Text>
-          </TouchableOpacity>
-        )}
+        <TouchableOpacity style={styles.socialButton}>
+          <Image source={require('../images/FBIcon.png')} style={styles.socialIcon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image source={require('../images/GoogleIcon.png')} style={styles.socialIcon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image source={require('../images/WhatsApp.png')} style={styles.socialIcon} />
+        </TouchableOpacity>
       </View>
-      <View style={styles.menuContainer}>
-        {!isLoggedIn && (
-          <TouchableOpacity onPress={handleCreateAccountPress} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Create Account</Text>
-          </TouchableOpacity>
-        )}
-        {!isLoggedIn && (
-          <TouchableOpacity onPress={handleLoginPress} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Login</Text>
-          </TouchableOpacity>
-        )}
-        {isLoggedIn && (
-          <TouchableOpacity onPress={handleLogoutPress} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Logout</Text>
-          </TouchableOpacity>
-        )}
+      <View style={styles.signupContainer}>
+        <Text>Don't have an account?</Text>
+        <TouchableOpacity>
+          <Text style={styles.signupText}>Sign Up!</Text>
+        </TouchableOpacity>
       </View>
-    </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#fff',
-  },
-  dashboardContainer: {
+  container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#EAEAEA',
-    position: 'relative',
+    padding: 20,
+    backgroundColor: '#f0f0f0',
   },
-  profileContainer: {
-    position: 'absolute',
-    top: 20,
-    left: 20,
-    borderRadius: 50,
-    overflow: 'hidden',
+  header: {
+    marginBottom: 40,
   },
-  avatar: {
-    width: 100,
-    height: 100,
-  },
-  dashboardText: {
+  welcomeText: {
     fontSize: 24,
     fontWeight: 'bold',
   },
-  loginButton: {
-    padding: 10,
-    backgroundColor: '#5CB85C',
+  inputContainer: {
+    width: '100%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    marginBottom: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
     borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  loginButton: {
+    backgroundColor: '#e91e63',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginBottom: 10,
   },
   loginButtonText: {
     color: '#fff',
+    textAlign: 'center',
     fontWeight: 'bold',
   },
-  menuContainer: {
+  forgotPasswordText: {
+    color: '#e91e63',
+    marginBottom: 20,
+  },
+  socialLoginContainer: {
+    flexDirection: 'row',
+  },
+  socialButton: {
+    marginHorizontal: 10,
+  },
+  socialIcon: {
+    width: 35,
+    height: 35,
+  },
+  signupContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     marginTop: 20,
   },
-  menuItem: {
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  menuItemText: {
-    fontSize: 18,
+  signupText: {
+    color: '#e91e63',
+    marginLeft: 5,
   },
 });
 
