bd0692e3a34b264652a485a0b935532f23d0ae53
added stat color <littleseon@gmail.com>
2023-11-30 04:59:30 -0400

    restructure Navigation

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import HomeScreen from '../Screens/HomeScreen';
+import HomeStackNavigator from './StackNavigator'; // Newly added
 import HelpScreen from '../Screens/HelpScreen';
 import MenuScreen from '../Screens/MenuScreen';
-import ProfileStackNavigator from './ProfileStackNavigator'; // This should return a valid React component
-import ProfileScreen from '../Screens/ProfileScreen';
-import BenefitsScreen from '../Screens/BenefitsScreen';
-import PackagesScreen from '../Screens/PackagesScreen';
+import ProfileStackNavigator from './ProfileStackNavigator';
 
 const Tab = createBottomTabNavigator();
 
-// This component sets up the bottom tab navigator with four tabs: Home, Profile, Help, and Menu.
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
-    screenOptions={({ route }) => ({
+      screenOptions={({ route }) => ({
       headerShown: false, // This will hide the header
       tabBarIcon: ({ focused, color, size }) => {
           let iconName;
@@ -28,19 +24,17 @@ const BottomTabNavigator = () => {
           } else if (route.name === 'Menu') {
             iconName = focused ? 'ios-menu' : 'ios-menu-outline';
           }
-          // 'as keyof typeof Ionicons.glyphMap' is used to ensure type safety for icon names.
+    
           return <Ionicons name={iconName as keyof typeof Ionicons.glyphMap} size={size} color={color}/>;
         },
         tabBarActiveTintColor: 'tomato',
         tabBarInactiveTintColor: 'gray',
       })}
     >
-      <Tab.Screen name="Home" component={HomeScreen}/>
-      <Tab.Screen name="Profile" component={ProfileStackNavigator}/> 
+      <Tab.Screen name="Home" component={HomeStackNavigator}/>
+      <Tab.Screen name="Profile" component={ProfileStackNavigator}/>
       <Tab.Screen name="Help" component={HelpScreen}/>
       <Tab.Screen name="Menu" component={MenuScreen}/>
-      <Tab.Screen name = "BenefitsScreen" component={BenefitsScreen}/>
-      <Tab.Screen name ="PackagesScreen" component={PackagesScreen}/>
     </Tab.Navigator>
   );
 };
--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,29 +1,25 @@
+// StackNavigator.tsx
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from './navigationTypes';
-import HomeScreen from '../Screens/HomeScreen';
+import HomeScreen from '../Screens/HomeScreen'; // Correctly imported
 import HelpScreen from '../Screens/HelpScreen';
 import MenuScreen from '../Screens/MenuScreen';
-import BenefitsScreen from '../Screens/BenefitsScreen';
 import DetailedInfoScreen from '../Screens/DetailedInfoScreen';
+import BenefitsScreen from '../Screens/BenefitsScreen';
 import PackagesScreen from '../Screens/PackagesScreen';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 function StackNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen}/>
       <Stack.Screen name="HelpScreen" component={HelpScreen}/>
       <Stack.Screen name="MenuScreen" component={MenuScreen}/>
-      <Stack.Screen name="BenefitsScreen" component={BenefitsScreen}/>
       <Stack.Screen name="DetailedInfoScreen" component={DetailedInfoScreen}/>
-      <Stack.Screen name="PackagesScreen" component={PackagesScreen}/>
-  
+      <Stack.Screen name = "BenefitsScreen" component = {BenefitsScreen}/>
+      <Stack.Screen name = "PackagesScreen" component = {PackagesScreen}/>
     </Stack.Navigator>
   );
 }
